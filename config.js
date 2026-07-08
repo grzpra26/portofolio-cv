18b2b493a5da13fc46092c906d7486c86535055b
grzpra26 <104147671+grzpra26@users.noreply.github.com>
2026-07-09 00:39:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // OPTION 1: Leave empty to use local content.json
 // OPTION 2: Paste your Google Apps Script Web App URL here to make this website editable from Google Sheets.
 // Example: window.CMS_URL = "https://script.google.com/macros/s/XXXX/exec";
-window.CMS_URL = "https://script.google.com/macros/s/AKfycbwqt3m0Ae97EBe2TUpOkFsrziotLBuU1KvEwNiHxG_7ZOcJTDlqefRXYeIaOrz5Hgawjg/exec";
+window.CMS_URL = "https://script.google.com/macros/s/AKfycbxeiCltpJCT0N1Lg9EyKbfIxusAp8l-hLvKr5nQczfsKmg5PyotzgxxyQJNtkvCt6Gt/exec";
